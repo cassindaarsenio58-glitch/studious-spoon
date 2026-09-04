500aa23f894e51c85cfbbf54a2b08d002d03edcc
cassindaarsenio58-glitch <cassindaarsenio58@gmail.com>
2026-09-04 11:03:21 +0100

    Create Tuplas.py

---
+++ Tuplas.py
@@ -0,0 +1,10 @@
+dias_uteis = ("Segunda", "Terça", "Quarta", "Quinta", "Sexta")
+
+print("Primeiro dia útil:", dias_uteis[0])
+print("Último dia útil:", dias_uteis[-1])
+
+print("Total de dias úteis:", len(dias_uteis))
+
+print("\nLista de dias:")
+for dia in dias_uteis:
+    print(f"- {dia}")
